ab257da9091bfc9564d75c8b342d7293ba4df0ce
Unknown <crisgonmon@hotmail.com>
2018-06-07 17:43:47 +0200

    Casos test pedido peligroso superados

--- ex dual junio eclipse/examen Dual junio 2018/ExJunio/src/main/java/org/mvpigs/commandpattern/pedidos/PedidoPeligrosoOrden.java
+++ ex dual junio eclipse/examen Dual junio 2018/ExJunio/src/main/java/org/mvpigs/commandpattern/pedidos/PedidoPeligrosoOrden.java
@@ -1,5 +1,30 @@
 package org.mvpigs.commandpattern.pedidos;
 
-public class PedidoPeligrosoOrden {
+import interfaces.PedidoPeligroso;
+
+public class PedidoPeligrosoOrden implements PedidoPeligroso {
+
+	private String destino;
+	private String instrucciones;
+	
+	public PedidoPeligrosoOrden(String destino, String instrucciones) {
+		this.destino = destino;
+		this.instrucciones = instrucciones;
+	}
+
+	@Override
+	public int peso() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String destino() {
+		// TODO Auto-generated method stub
+		return this.destino;
+	}
+	public String instrucciones() {
+		return this.instrucciones;
+	}
 
 }
--- ex dual junio eclipse/examen Dual junio 2018/ExJunio/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
+++ ex dual junio eclipse/examen Dual junio 2018/ExJunio/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
@@ -1,5 +1,23 @@
 package org.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoPeligroso {
+import interfaces.PedidoPeligroso;
+import interfaces.TratamientoPedido;
+
+public class TratamientoPedidoPeligroso implements TratamientoPedido {
+	private PedidoPeligroso pedido;
+	
+	public TratamientoPedidoPeligroso(PedidoPeligroso pedido) {
+		this.pedido = pedido;
+	}
+
+	@Override
+	public boolean tratar() {
+		if (this.pedido.instrucciones().equals("no ponerselo en el dedo")) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 
 }
